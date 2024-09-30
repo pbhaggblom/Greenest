084a94896efce1c41ecc10fe149744b860fe6e90
Pontus Häggblom <pbhaggblom@gmail.com>
2024-09-30 16:02:55 +0200

    Skapar konstruktor i Main för att komma ur den statiska kontexten

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public Main() {
         Kaktus k = new Kaktus("p", 1.5);
 
         System.out.println(k.getHöjdIMeter());
+    }
 
+    public static void main(String[] args) {
+        Main m = new Main();
     }
 }
